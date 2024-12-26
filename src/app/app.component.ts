0294c6743d0a17ea46bc91b0f986a081b3ec124d
GranCacioRomano <falconetti.grafico@gmail.com>
2024-12-26 13:19:09 +0100

    Html e Css Per la pagina di ricerca birra.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SearchBeerComponent } from "./beer/search-beer/search-beer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SearchBeerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
