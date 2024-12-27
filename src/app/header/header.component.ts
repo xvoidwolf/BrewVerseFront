03329f8c3dcad39288492999c08f6197d2d9142a
GranCacioRomano <falconetti.grafico@gmail.com>
2024-12-27 17:08:30 +0100

    Aggiustamento NavBar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
 }
