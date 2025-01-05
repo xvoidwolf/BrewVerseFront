e07aea8d7a6e0a4af55eb9402e1451a34595a726
Eleonora <eleonorasiciliano99@gmail.com>
2025-01-05 18:11:08 +0100

    inizia ts review

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-review',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './review.component.html',
   styleUrl: './review.component.css'
 })
-export class ReviewComponent {
+export class ReviewComponent implements OnInit{
+ reviewForm!:FormGroup;
 
+ constructor(private fb: FormBuilder, private reviewService:ReviewService, private router:Router){ }
+
+ ngOnInit(): void{
+  this.reviewForm=this.fb.group({
+  text:['',[Validators.required, Validators.minLength(10), Validators.maxLength(240)]],
+  rating:['',[Validators.required]]  
+})
+ }
+ onSubmit(){
+  this.reviewService
+ }
 }
