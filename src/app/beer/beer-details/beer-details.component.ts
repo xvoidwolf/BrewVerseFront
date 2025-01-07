fd72ee8e301a0df997fe365ab5b3682c996b2a28
Eleonora <eleonorasiciliano99@gmail.com>
2025-01-07 10:07:01 +0100

    crea reviewcard component

--- src/app/beer/beer-details/beer-details.component.ts
+++ src/app/beer/beer-details/beer-details.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { ReviewService } from '../../services/review.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Review } from '../../model/review';
+import { ReviewCardComponent } from '../../review-card/review-card.component';
 
 @Component({
   selector: 'app-beer-details',
-  imports: [],
+  imports: [ReviewCardComponent],
   templateUrl: './beer-details.component.html',
   styleUrl: './beer-details.component.css'
 })
 export class BeerDetailsComponent {
+reviews!:Review[];
+beerId!:number;
+
+constructor(private reviewService:ReviewService, private router:Router, private route:ActivatedRoute){ }
+
+
+ngOnInit():void{
+  this.getReviews();
+}
+
+getReviews(){
+  this.beerId=Number(this.route.snapshot.paramMap.get('id'));
+  this.reviewService.getReviews(this.beerId).subscribe({
+    next: reviews=>{
+      this.reviews=reviews;
+    },
+    error:err=>{
+      console.log("errore nel caricamento delle reviews",err);
+    }
+  });
+}
 
 }
