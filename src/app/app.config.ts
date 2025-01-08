91b8d6d8bbeb8131530320c222600a678c0e6c36
Stefano Puro <stefano_stefano.puro@hotmail.com>
2025-01-08 12:13:16 +0100

    aggiornato vetrina con carosello

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './security/auth-interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
+  providers: [provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    }),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
     provideHttpClient((withInterceptors([authInterceptor])))]
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,16 +3,38 @@ import { Beer } from '../model/beer';
 import { ShowcaseService } from '../services/showcase.service';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule,CarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit { 
   beers: Beer[] = [];
-  
+  responsiveOptions: any[] | undefined = [
+    {
+        breakpoint: '1400px',
+        numVisible: 2,
+        numScroll: 1,
+    },
+    {
+        breakpoint: '1199px',
+        numVisible: 3,
+        numScroll: 1,
+    },
+    {
+        breakpoint: '767px',
+        numVisible: 2,
+        numScroll: 1,
+    },
+    {
+        breakpoint: '575px',
+        numVisible: 1,
+        numScroll: 1,
+    },
+    ]; 
 
   constructor(private showcaseService: ShowcaseService) { }
 
@@ -29,5 +51,5 @@ export class HomeComponent implements OnInit {
       }
     });
   }
-  
+
 }
