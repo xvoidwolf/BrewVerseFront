e9321d5e5630f241173f69446e8973f602709774
Stefano Puro <stefano_stefano.puro@hotmail.com>
2025-01-08 17:38:14 +0100

    mergio branch stefano nella master

--- src/app/beer/beer-card/beer-card.component.ts
+++ src/app/beer/beer-card/beer-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Beer } from '../../model/beer';
-import { BeerDetailsComponent } from '../beer-details/beer-details.component';
+
 
 @Component({
   selector: 'app-beer-card',
@@ -8,7 +8,7 @@ import { BeerDetailsComponent } from '../beer-details/beer-details.component';
   templateUrl: './beer-card.component.html',
   styleUrl: './beer-card.component.css'
 })
-export class BeerCardComponent implements OnInit {
+export class BeerCardComponent {
   @Input()
   beer!:Beer;
 
