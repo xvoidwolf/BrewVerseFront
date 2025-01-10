af1c01a86a000080b6630ba5a01b6c8639a75a49
Eleonora <eleonorasiciliano99@gmail.com>
2025-01-10 12:57:44 +0100

    implementa onDeleteReview

--- src/app/beer-review-card/beer-review-card.component.ts
+++ src/app/beer-review-card/beer-review-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { BeerReview } from '../model/beer-review';
+import { ReviewService } from '../services/review.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-beer-review-card',
@@ -11,4 +13,22 @@ export class BeerReviewCardComponent {
   @Input()
   beerReview!:BeerReview;
 
+  constructor(private reviewService:ReviewService, private router:Router){}
+
+  onEdit(reviewId:number){
+    this.router.navigate([`edit-review/${reviewId}`]);
+  }
+  onDelete(reviewId:number){
+    this.reviewService.deleteReview(reviewId).subscribe({
+      next:() => {
+        alert('The review was deleted successfully!');
+        this.router.navigate([`home`]);
+
+      },
+      error :(error) => {
+        console.log(error);
+        alert('The review could not be deleted!');
+      }
+    });
+  }
 }
