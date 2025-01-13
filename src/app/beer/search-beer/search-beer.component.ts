1b01fa1afb483d953ddb9c3e3727694f15991634
Eleonora <eleonorasiciliano99@gmail.com>
2025-01-13 16:39:39 +0100

    implementa search bear: aggiunge componente esterna pager e valida la form se almeno un campo è selezionato

--- src/app/beer/search-beer/search-beer.component.ts
+++ src/app/beer/search-beer/search-beer.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { BeerService } from '../../services/beer.service';
 import { Beer } from '../../model/beer';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BeerCardSearchComponent } from "../beer-card-search/beer-card-search.component";
+import { PagerComponent } from "../../pager/pager.component";
 
 @Component({
-  selector: 'app-search-beer', 
-  imports: [ReactiveFormsModule, BeerCardSearchComponent],
+  selector: 'app-search-beer',
+  imports: [ReactiveFormsModule, BeerCardSearchComponent, PagerComponent],
   templateUrl: './search-beer.component.html',
   styleUrls: ['./search-beer.component.css']
 })
@@ -17,33 +18,33 @@ export class SearchBeerComponent implements OnInit {
   beers: Beer[] = [];
   searchExecuted: boolean = false;
   totalElements: number = 0;
+  totalPages:number = 0;
   pageNumber: number = 0;
-  pageSize: number = 12;
+  pageSize: number = 3;
   alcoholRange: string = '';
   ratingRange: string = '';
   breweryId: number = 0;
   beerNames: string[] = [];
-  type: string= '';
-  totalItems!: number;
+  type: string = '';
   paginatedBeers: any[] = [];
   beerTypes: string[] = [];
   breweries: any[] = [];
   beerName: string = '';
 
-  constructor(private fb:FormBuilder, private beerService: BeerService, private router:Router, private route:ActivatedRoute) {}
-  
+  constructor(private fb: FormBuilder, private beerService: BeerService, private router: Router, private route: ActivatedRoute) { }
+
   ngOnInit(): void {
-    this.beerForm= this.fb.group({
-      beerName:['',[]],
-      breweryId:['',[]],
-      type:['',[]],
-      alcoholRange:['',[]],
-      ratingRange:['',[]],
+    this.beerForm = this.fb.group({
+      beerName: ['', []],
+      breweryId: ['', []],
+      type: ['', []],
+      alcoholRange: ['', []],
+      ratingRange: ['', []],
     });
 
     this.beerService.getBeerTypes().subscribe({
       next: (data) => {
-        this.beerTypes = data; 
+        this.beerTypes = data;
       },
       error: (error) => {
         console.error('Errore durante il caricamento dei tipi di birre:', error);
@@ -57,7 +58,7 @@ export class SearchBeerComponent implements OnInit {
       error: (error) => {
         console.error('Errore durante il caricamento dei nomi delle birre:', error);
       },
-  });
+    });
 
     this.beerService.getBreweries().subscribe({
       next: (data) => {
@@ -68,9 +69,9 @@ export class SearchBeerComponent implements OnInit {
       },
     });
   }
-  
+
   onSubmit(event: Event): void {
-    if(this.beerForm.valid){
+    if (this.beerForm.valid) {
       this.searchExecuted = true;
       this.pageNumber = 0;
       this.ratingRange = this.beerForm.get('ratingRange')?.value || null;
@@ -88,14 +89,16 @@ export class SearchBeerComponent implements OnInit {
         this.type,
         this.pageSize,
         this.pageNumber).subscribe({
-        next: (response:any) => {
-          this.beers = response.content;
-          console.log(response);
-        },
-        error: (error) => {
-          console.error('Errore durante il caricamento delle birre:', error);
-        }
-      });
+          next: (response: any) => {
+            this.beers = response.content;
+            this.totalElements = response.totalElements;
+            this.totalPages = response.totalPages;
+            console.log(response);
+          },
+          error: (error) => {
+            console.error('Errore durante il caricamento delle birre:', error);
+          }
+        });
     }
   }
 
@@ -108,25 +111,16 @@ export class SearchBeerComponent implements OnInit {
   }
 
   goToNextPage() {
-    if(this.beerForm.valid){
-      this.searchExecuted = true;
-      this.pageNumber ++;
-      this.ratingRange = this.beerForm.get('ratingRange')?.value || null;
-      this.alcoholRange = this.beerForm.get('alcoholRange')?.value || null;
-      this.breweryId = this.beerForm.get('breweryId')?.value || null;
-      console.log(this.beerForm.get('breweryId'));
-      this.type = this.beerForm.get('type')?.value || null;
-      this.beerName = this.beerForm.get('beerName')?.value || null;
-      console.log(this.beerForm.value);
-      this.beerService.getBeers(
-        this.beerName,
-        this.ratingRange,
-        this.alcoholRange,
-        this.breweryId,
-        this.type,
-        this.pageSize,
-        this.pageNumber).subscribe({
-        next: (response:any) => {
+    this.pageNumber++;
+    this.beerService.getBeers(
+      this.beerName,
+      this.ratingRange,
+      this.alcoholRange,
+      this.breweryId,
+      this.type,
+      this.pageSize,
+      this.pageNumber).subscribe({
+        next: (response: any) => {
           this.beers = response.content;
           console.log(response);
         },
@@ -134,28 +128,19 @@ export class SearchBeerComponent implements OnInit {
           console.error('Errore durante il caricamento delle birre:', error);
         }
       });
-    }
   }
+
   goToPreviousPage() {
-    if(this.beerForm.valid){
-      this.searchExecuted = true;
-      this.pageNumber --;
-      this.ratingRange = this.beerForm.get('ratingRange')?.value || null;
-      this.alcoholRange = this.beerForm.get('alcoholRange')?.value || null;
-      this.breweryId = this.beerForm.get('breweryId')?.value || null;
-      console.log(this.beerForm.get('breweryId'));
-      this.type = this.beerForm.get('type')?.value || null;
-      this.beerName = this.beerForm.get('beerName')?.value || null;
-      console.log(this.beerForm.value);
-      this.beerService.getBeers(
-        this.beerName,
-        this.ratingRange,
-        this.alcoholRange,
-        this.breweryId,
-        this.type,
-        this.pageSize,
-        this.pageNumber).subscribe({
-        next: (response:any) => {
+    this.pageNumber--;
+    this.beerService.getBeers(
+      this.beerName,
+      this.ratingRange,
+      this.alcoholRange,
+      this.breweryId,
+      this.type,
+      this.pageSize,
+      this.pageNumber).subscribe({
+        next: (response: any) => {
           this.beers = response.content;
           console.log(response);
         },
@@ -163,10 +148,18 @@ export class SearchBeerComponent implements OnInit {
           console.error('Errore durante il caricamento delle birre:', error);
         }
       });
-    }
   }
 
   onCardSelected(beer: Beer) {
-  this.router.navigate([`details/${beer.id}`]);
- }
+    this.router.navigate([`details/${beer.id}`]);
+  }
+
+  isButtonDisabled() : boolean {
+    const formValues=Object.values(this.beerForm.value);  
+    return !formValues.some(v=>v && (<string> v).trim() !== "");
+    //some = metodo come .stream da chiamare suglia array
+    //trim = metodo sulle stringhe
+    //v non è nullo ed è diverso dalla stringa ""
+    //ritorna che non è vero che almeno uno degli elementi esiste ed è diverso dalla stringa vuota -> il bottone deve essere disabilitato
+  }
 }
--- src/app/pager/pager.component.ts
+++ src/app/pager/pager.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pager',
@@ -8,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class PagerComponent {
 
+  @Input()
+  pageNumber!: number;
+  @Input()
+  totalPages!: number;
+  @Input()
+  totalElements!: number;
+  @Output()
+  next = new EventEmitter<number>();
+  @Output()
+  previous = new EventEmitter<number>();
+
+  isFirstPage(): boolean {
+    return this.pageNumber === 0;
+  }
+
+  isLastPage(): boolean {
+    return this.pageNumber === this.totalPages - 1;
+  }
+
+  goToNextPage() {
+   this.next.emit(this.pageNumber);
+  }
+
+  goToPreviousPage() {
+    this.previous.emit(this.pageNumber);
+  }
 }
